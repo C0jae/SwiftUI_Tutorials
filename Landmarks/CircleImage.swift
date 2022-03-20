1dfa3b15459609f163505fba6cb86f8a54e68bd4
Youngjae Choi <cdw0807@naver.com>
2022-03-20 21:19:55 +0900

    "22.03.20" VStack, HStack, Mapkit, @State

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -2,26 +2,24 @@
 //  CircleImage.swift
 //  Landmarks
 //
-//  Created by 최영재 on 2022/03/16.
+//  Created by 최영재 on 2022/03/20.
 //
 
 import SwiftUI
 
 struct CircleImage: View {
-    var image: Image
-    
     var body: some View {
-        image
-            .clipShape(Circle())
-            .overlay {
+        Image("turtlerock")
+            .clipShape(Circle())    // 이미지 모양 원으로 설정
+            .overlay {  // 테두리(?) 설정
                 Circle().stroke(.white, lineWidth: 4)
             }
-            .shadow(radius: 7)
+            .shadow(radius: 7)  // 명암
     }
 }
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage(image: Image("tutlerock"))
+        CircleImage()
     }
 }
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -2,15 +2,27 @@
 //  ContentView.swift
 //  Landmarks
 //
-//  Created by 최영재 on 2022/03/16.
+//  Created by 최영재 on 2022/03/20.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
